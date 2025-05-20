a0cb26cc4bfd56cffc6864ecf9f6187b270c412e
moonc4ke <adom.donatas@gmail.com>
2025-05-20 17:35:57 +0100

    add Comment.nvim plugin configuration to always force line comment style

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    config = function()
+      require('Comment').setup({
+        -- This will ensure we always use single-line comments, not block comments
+        pre_hook = function(ctx)
+          -- Only calculate commentstring for languages that might have block comments
+          local U = require('Comment.utils')
+          
+          -- Always force line comment style (single-line comments)
+          -- ctx.ctype = U.ctype.line
+          return require('Comment.ft').calculate(ctx, { ctype = U.ctype.line }) or vim.bo.commentstring
+        end,
+      })
+    end,
+  }
+} 
