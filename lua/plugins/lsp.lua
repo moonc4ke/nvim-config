56c95e6c7dab6a0792ba201abc396ecf873883a1
moonc4ke <adom.donatas@gmail.com>
2025-07-18 20:36:51 +0300

    add codeium, lsp support, hidden files view and vue syntax and lsp support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,146 @@
+-- LSP Configuration
+
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ts_ls",        -- TypeScript/JavaScript LSP
+          "angularls",    -- Angular LSP
+          "volar",        -- Vue.js LSP
+          "html",         -- HTML LSP
+          "cssls",        -- CSS LSP
+          "eslint",       -- ESLint LSP
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- TypeScript/JavaScript LSP
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+        settings = {
+          typescript = {
+            preferences = {
+              disableSuggestions = false,
+            },
+          },
+        },
+      })
+
+      -- Angular LSP
+      lspconfig.angularls.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
+        root_dir = lspconfig.util.root_pattern("angular.json", "project.json"),
+      })
+
+      -- HTML LSP
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html" },
+      })
+
+      -- CSS LSP
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        filetypes = { "css", "scss", "less" },
+      })
+
+      -- Vue.js LSP
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = { "vue" },
+        init_options = {
+          vue = {
+            hybridMode = false,
+          },
+        },
+      })
+
+      -- ESLint LSP
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "vue" },
+      })
+
+      -- Key mappings for LSP
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover documentation' })
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end,
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = {"lua", "javascript", "ruby", "php", "typescript", "css", "scss", "html", "embedded_template"},
+      ensure_installed = {"lua", "javascript", "ruby", "php", "typescript", "css", "scss", "html", "embedded_template", "json", "tsx", "angular", "vue"},
       highlight = { enable = true },
       indent = { enable = true }
     })
