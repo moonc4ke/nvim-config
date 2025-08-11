b26f9239b280b8ef6b765fedb398ba5c01348fb9
moonc4ke <adom.donatas@gmail.com>
2025-08-11 13:37:50 +0300

    add vue treesitter support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = {"lua", "javascript", "ruby", "php", "typescript", "css", "scss", "html", "embedded_template", "json", "tsx", "angular", "latex", "norg", "svelte", "typst", "regex"},
+      ensure_installed = {"lua", "javascript", "ruby", "php", "typescript", "css", "scss", "html", "embedded_template", "json", "tsx", "angular", "latex", "norg", "svelte", "typst", "regex", "vue"},
       highlight = { enable = true },
       indent = { enable = true },
       sync_install = false,
