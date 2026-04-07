4e6f0d8fe97220060bcc69080917a6feeedec943
moonc4ke <adom.donatas@gmail.com>
2026-04-07 10:53:09 +0300

    fix nvim treesitter issues

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,28 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    local ensure_installed = {
+      "lua", "javascript", "ruby", "php", "typescript", "css", "scss",
+      "html", "embedded_template", "json", "tsx", "angular", "latex",
+      "svelte", "typst", "regex", "vue",
+    }
 
-    configs.setup({
-      ensure_installed = {"lua", "javascript", "ruby", "php", "typescript", "css", "scss", "html", "embedded_template", "json", "tsx", "angular", "latex", "norg", "svelte", "typst", "regex", "vue"},
-      highlight = { enable = true },
-      indent = { enable = true },
-      sync_install = false,
-      auto_install = true,
-      matchup = {
-        enable = true, -- Enable vim-matchup integration
-      },
+    local installed = require("nvim-treesitter.config").get_installed()
+    local to_install = vim.tbl_filter(function(lang)
+      return not vim.tbl_contains(installed, lang)
+    end, ensure_installed)
+
+    if #to_install > 0 then
+      require("nvim-treesitter.install").install(to_install)
+    end
+
+    -- Enable treesitter highlighting for all buffers with a parser
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
     })
 
     vim.treesitter.language.register("html", "hbs")
